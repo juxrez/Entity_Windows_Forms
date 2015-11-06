2e765dd1ad4d9d0f5be0607a0483f8d169033072
Alexis <alexis.juarez@msw.com.mx>
2015-11-06 10:06:54 -0600

    pull data from database to textbox

---
+++ entityWindowsForms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entityWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        allisonEntities db = new allisonEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = 1;
+            rol user = new rol();
+            user = db.rols.FirstOrDefault(u => u.id_rol == id);
+            txtNombre.Text = user.rol_nombre;
+            txtApellido.Text = user.id_roles_identity;
+           
+        }
+    }
+}
